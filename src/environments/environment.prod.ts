8d204207ac9eb8760c3c3a6d7192dfe8dbfa2274
Ard Ademi (MacOS) <ard.ademi@raiffeisen-kosovo.com>
2020-12-09 22:49:22 +0100

    add env variables in environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dev-eu-andyx-buildersoft.azurewebsites.net'
+  apiUrl: 'https://dev-eu-andyx-buildersoft.azurewebsites.net/api/v1',
+  signalRUrl: 'https://dev-eu-andyx-buildersoft.azurewebsites.net/realtime/v1/dashboard',
 };
